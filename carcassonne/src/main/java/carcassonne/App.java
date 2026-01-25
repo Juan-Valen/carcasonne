8b391cbf702bfe7a6f4945dec5d211a9d736b96e
john <juan.valenz.101@hotmail.com>
2026-01-25 23:04:51 +0200

    Add maven template to start app development

---
+++ carcassonne/src/main/java/carcassonne/App.java
@@ -0,0 +1,13 @@
+package carcassonne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
