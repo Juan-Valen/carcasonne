41d147b87f0c9e71de265e84ae86533a14653750
NooaV-M <nooa.vainio-mattila@metropolia.fi>
2026-03-09 17:16:30 +0200

    Add database connection and grant privileges for user access

--- config/init.sql
+++ config/init.sql
@@ -2,6 +2,8 @@ CREATE USER carcas WITH PASSWORD 'carcassonne1';
 CREATE DATABASE carcassonne;
 GRANT ALL PRIVILEGES ON DATABASE carcassonne TO carcas;
 
+\c carcassonne;
+
 CREATE TABLE users
 (
   user_id SERIAL NOT NULL,
@@ -29,7 +31,10 @@ CREATE TABLE saves
   FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-INSERT INTO users(username, password) 
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO carcas;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO carcas;
+
+INSERT INTO users(username, password)
 VALUES
     ('carcassonne', 'password'),
     ('juan', '0j#mi9-fd2j_k4'),
