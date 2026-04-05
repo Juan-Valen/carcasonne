6a56d6c8ec8f273a006573e1244e4d5bceeb077d
R.For_Swan <rfaure@et.esiea.fr>
2026-04-05 15:12:27 +0300

    Cleaned code

--- carcassonne/src/main/java/carcassonne/Model/Language.java
+++ carcassonne/src/main/java/carcassonne/Model/Language.java
@@ -4,28 +4,16 @@ import carcassonne.Service.databaseService;
 import java.util.*;
 
 public class Language {
-//    public Locale currentLocale;
-//    public ResourceBundle resourceBundle;
-
     private databaseService DBSI = databaseService.getInstance();
 
     private String currentLanguage = "en";
     private Map<String, Map<String, String>> languages = new HashMap<>();
 
-
-
-
     public Language() {
-//        this.currentLocale = new Locale("en");
-//        this.resourceBundle = ResourceBundle.getBundle("bundle", currentLocale);
         languages.put(currentLanguage, DBSI.getTranslations(currentLanguage));
-
-
     }
 
     public void setLanguage(String languageCode) {
-//        this.currentLocale = new Locale(languageCode);
-//        this.resourceBundle = ResourceBundle.getBundle("bundle", currentLocale);
         currentLanguage = languageCode;
         if(!languages.containsKey(languageCode)) {
             languages.put(languageCode, DBSI.getTranslations(languageCode));
@@ -33,7 +21,6 @@ public class Language {
     }
 
     public String getString(String key) {
-//        return resourceBundle.getString(key);
         Map<String, String> translations = languages.get(currentLanguage);
         if(translations.containsKey(key)) return translations.get(key);
         return languages.get("en").get(key);
